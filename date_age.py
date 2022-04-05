dc8d574aaeed0889a8151738e3b4ea4a8ea639c5
Yiyun Qin <yiyun.qin@mths.ca>
2022-04-05 23:44:29 +0000

    date age

--- date_age.py
+++ date_age.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Created by Yiyun Qin
-# Created on April 2022
+# Created in April 2022
 # This is the math program, showing whether your age is acceptable for the date
 
 
@@ -18,7 +18,7 @@ def main():
         if age_int > 25 and age_int < 40:
             print("You are an acceptable age.")
         else:
-            print("You are not an acceptable age!")
+            print("You are NOT an acceptable age!")
     except Exception:
         print("{} is not a valid answer!".format(age))
     finally:
